0798ce5ad52fd2ac4b8d69359ea27cc7788f0373
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-08 00:51:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ string = "Hello there, and how are you?"
 def reverse_each_word (string)
   array = string.split
   array.collect do |words|
-   rev_array = words.reverse
-   rev_array.join
+   (words.reverse).join
+   
   end
 end
